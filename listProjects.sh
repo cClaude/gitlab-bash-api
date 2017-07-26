e7e5bd3d7dd1adc699e2df6aab919c1ae7f456b7
cClaude <claude.choisnet@gmail.com>
2017-07-26 16:24:31 +0200

    Allow to list project by name

--- listProjects.sh
+++ listProjects.sh
@@ -3,6 +3,10 @@
 # Documentation:
 #   https://docs.gitlab.com/ce/api/projects.html#get-single-project
 #
+function display_usage {
+  echo "Usage: $0 --all | --name PROJECT_NAME | --id PROJECT_ID [--raw]" >&2
+  exit 100
+}
 
 # Configuration
 if [ -z "$GITLAB_BASH_API_PATH" ]; then
@@ -18,16 +22,71 @@ source "${GITLAB_BASH_API_PATH}/api/gitlab-bash-api.sh"
 
 # Script start here
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 [--all | PROJECT_ID]" >&2
-  exit 100
+  display_usage
 fi
 
 # Parameters
-if [ "$1" = "--all" ]; then
-  PROJECT_ID=
+PROJECT_ID=
+PROJECT_NAME=
+RAW=false
+
+while [[ $# > 0 ]]
+do
+param="$1"
+shift
+case $param in
+    -a|--all)
+        ;;
+        
+    -n|--name)
+        PROJECT_NAME="$1"
+        shift
+        ;;
+        
+    -i|--id)
+        PROJECT_ID="$1"
+        shift
+        ;;
+        
+    -r|--raw)
+        RAW=true
+        ;;
+        
+    *)
+        # unknown option
+        display_usage
+        ;;
+esac
+done
+
+if [ "${RAW}" = "true" ] ; then
+  answer=$(list_projects_raw "${PROJECT_ID}" '')
 else
-  PROJECT_ID=$1
+  answer=$(list_projects "${PROJECT_ID}" '')
 fi
 
-LIST_PROJECT=$(list_projects "${PROJECT_ID}" '')
-echo "${LIST_PROJECT}"
+if [ -z "${PROJECT_NAME}" ] ; then
+  echo "${answer}" | jq .
+else 
+  if [ "${RAW}" = "true" ] ; then
+    JQ_FILTER="[.[] | select(.name==\"${PROJECT_NAME}\")]"
+  else
+    JQ_FILTER="[.[] | select(.project_name==\"${PROJECT_NAME}\")]"
+  fi
+
+  echo "${answer}" | jq "${JQ_FILTER}"
+fi
+
+# List all id
+# ./listProjects.sh --all | jq '. [] | .id'
+
+# List name and id
+# ./listProjects.sh --all | jq '[ . [] | { project_name: .name, project_id: .id } ]'
+
+# Number of projects visible for current user
+# ./listProjects.sh --all | jq '. | length'
+
+# 
+# ./listProjects.sh --all | jq "[.[] | select(.group_name==\"${GROUP_NAME}\")]"
+# ./listProjects.sh --all --raw | jq "[.[] | select(.namespace.path==\"${GROUP_NAME}\")]"
+
