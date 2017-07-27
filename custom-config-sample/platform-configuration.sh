988eb12c68ae557149197a3c4bfde9bb78306654
cClaude <claude.choisnet@gmail.com>
2017-07-27 20:55:49 +0200

    Improve configuration templates

---
+++ custom-config-sample/platform-configuration.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Configuration related to the platform
+#
+
+GITLAB_URL_PREFIX=https://gitlab
+
+# Optionnal
+
+# Require to clone using ssh
+#GITLAB_CLONE_SSH_PREFIX=git@192.168.10.200
+
+# You can also configure in you ~/.bashrc file
+#
+# export GITLAB_BASH_API_PATH='__YOUR_PATH_TO/gitlab-bash-api'
+# export GITLAB_BASH_API_CONFIG="${GITLAB_BASH_API_PATH}/my-config"
