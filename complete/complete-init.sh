ae4588d53ebfaf0c02aa004bb35deefb41de4eb9
cClaude <claude.choisnet@gmail.com>
2017-11-10 01:49:38 +0100

    shellcheck

--- complete/complete-init.sh
+++ complete/complete-init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ -z "${GITLAB_BASH_API_PATH}" ]; then
-  GITLAB_BASH_API_PATH=$(dirname $(dirname $(realpath "$0")))
+  GITLAB_BASH_API_PATH=$(dirname "$(dirname "$(realpath "$0")")")
 elif [ ! -f "${GITLAB_BASH_API_PATH}/api/gitlab-bash-api.sh" ]; then
   echo "*** Bad value GITLAB_BASH_API_PATH=${GITLAB_BASH_API_PATH}" >&2
   echo "Try to fix using current file name" >&2
 
-  GITLAB_BASH_API_PATH=$(dirname $(dirname $(realpath "$0")))
+  GITLAB_BASH_API_PATH=$(dirname "$(dirname "$(realpath "$0")")")
 fi
 
 if [ -f "${GITLAB_BASH_API_PATH}/api/gitlab-bash-api.sh" ]; then
